38d0a04a77c338ae19ff58681440727db4d89d4d
ibrown <ibrown@twitter.com>
2016-09-17 16:21:45 -0700

    table creation passing tests

--- src/warbler.erl
+++ src/warbler.erl
@@ -16,7 +16,7 @@ get_incidents(Token, TeamId)->
     get_incidents(Token, TeamId, []).
 
 get_incidents(Token, TeamId, Acc)->
-   {ok,{{"HTTP/1.1",200,"OK"}, Headers, Body}}= httpc:request(get,
+   {ok,{{"HTTP/1.1",200,"OK"}, _, Body}}= httpc:request(get,
 		  {lists:flatten(["https://api.pagerduty.com/incidents?date_range=all&team_ids%5B%5D=",
 				  TeamId,
 				  "&offset=",
@@ -86,6 +86,46 @@ get_incident_keys([H|T], Acc)->
 get_incident_keys([], Acc)->
     lists:sort(sets:to_list(sets:from_list(Acc))).
     
+get_weeks(Buckets)->    
+    lists:sort([Week || {Week,_} <- Buckets]).
+    
+create_table(Buckets)->    
+    create_table(Buckets, get_weeks(Buckets), get_incident_keys(Buckets), [[] || _ <- get_incident_keys(Buckets)]).    
+create_table(Buckets, [Week|T], Keys, Acc)->    
+    {Week, Incidents} = lists:keyfind(Week, 1, Buckets),
+    Column = lists:map(fun(X)->
+		      case lists:keyfind(X, 1, Incidents) of
+			  false ->
+			      0;
+			  {X,Count} ->
+			      Count
+		      end
+	      end, Keys),
+    NewAcc = lists:zipwith(fun(X,Y) -> lists:append(X, [Y]) end, Acc, Column), 
+    create_table(Buckets, T, Keys,NewAcc);
+create_table(Buckets, [], Keys, NewAcc)->
+    Weeks = get_weeks(Buckets),
+    Headers=lists:append([undefined], Weeks),
+    Body=lists:zipwith(fun(X, Y)->lists:append([Y], X) end, NewAcc, Keys),
+    lists:append([Headers], Body).
+
+add_column(Table, Column)->
+    add_column(Table,Column,[]).
+add_column([Row|Table],[NewValue|Column],Acc)->
+    NewRow= lists:append(Row, NewValue), 
+    NewAcc=lists:append(Acc, [NewRow]),
+    add_column(Table, Column, NewAcc);
+add_column([],[],Acc) ->
+    Acc.
+
+table_to_csv(Table)->    
+    ok.
+
+    
+    
+
+
+    
     
 	    
 
@@ -121,5 +161,60 @@ get_incident_keys_test() ->
     ],
     %% incident keys should be returned sorted
     ["bar_incident_key", "bat_incident_key", "baz_incident_key", "bing_incident_key", "foo_incident_key"] = get_incident_keys(Buckets).
+
+get_weeks_test() ->
+    Buckets = [
+	       {{2015,53},[{"bat_incident_key", 1},{"bar_incident_key", 4}, {"foo_incident_key", 1}]},
+	       {{2015,51},[{"bat_incident_key", 3},{"bar_incident_key", 1}, {"bing_incident_key", 1}]},
+	       {{2016,1},[{"bat_incident_key", 3},{"bar_incident_key", 1}, {"bing_incident_key", 1}]},
+	       {{2016,2},[{"bat_incident_key", 3},{"bar_incident_key", 1}, {"bing_incident_key", 1}]},
+	       {{2016,24},[{"bat_incident_key", 3},{"bar_incident_key", 1}, {"bing_incident_key", 1}]},
+	       {{2015,1},[{"bat_incident_key", 3},{"bar_incident_key", 1}, {"bing_incident_key", 1}]},
+	       {{2015,50},[{"baz_incident_key", 5},{"bar_incident_key", 1}]}
+	      ],
+
+    OrderedWeeks = [{2015,1},
+		    {2015,50},
+		    {2015,51},
+		    {2015,53},
+		    {2016,1},
+		    {2016,2},
+		    {2016,24}
+		   ],
+    OrderedWeeks = get_weeks(Buckets).
+    
+create_table_test()->
+    Buckets = [
+	       {{2015,53},[{"bat_incident_key", 1},{"bar_incident_key", 4}, {"foo_incident_key", 1}]},
+	       {{2015,51},[{"bat_incident_key", 3},{"bar_incident_key", 1}, {"bing_incident_key", 1}]},
+	       {{2015,50},[{"baz_incident_key", 5},{"bar_incident_key", 1}]}
+	      ],
+    Table = [
+	     [undefined, {2015,50}, {2015,51}, {2015,53}],
+	     ["bar_incident_key",1,1,4],
+	     ["bat_incident_key",0,3,1],
+	     ["baz_incident_key",5,0,0],
+	     ["bing_incident_key",0,1,0],
+	     ["foo_incident_key",0,0,1]
+	    ],
+    Table = create_table(Buckets).
+    
+
+table_to_csv_test()->
+    Table = [
+	     [undefined, {2015,50}, {2015,51}, {2015,53}],
+	     ["bar_incident_key",1,1,4],
+	     ["bat_incident_key",0,3,1],
+	     ["baz_incident_key",5,0,0],
+	     ["bing_incident_key",0,1,0],
+	     ["foo_incident_key",0,0,1]
+	    ],
+    Csv=",2015-50,2015-51,2015-53\nbar_incident_key,1,1,4\nbat_incident_key,0,3,1\nbaz_incident_key,5,0,0\nbing_incident_key,0,1,0\nfoo_incident_key,0,0,1",
+    Csv = table_to_csv(Table).
+
+%add_column_test()->
+%    Table=[[1,2],[4,5],[7,8]],
+%    Column = [3,6,9],
+%    [[1,2,3],[4,5,6],[7,8,9]]=add_column(Table, Column).
 
 -endif.
